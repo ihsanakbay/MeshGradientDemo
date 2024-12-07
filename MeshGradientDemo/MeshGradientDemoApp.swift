d76ee97f79e4e7bbba8db68be862d34377c41d4c
İhsan <ihsan.akbay@etiya.com>
2024-12-07 15:23:53 +0300

    Initial Commit

---
+++ MeshGradientDemo/MeshGradientDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshGradientDemoApp.swift
+//  MeshGradientDemo
+//
+//  Created by İhsan Akbay on 7.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MeshGradientDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
