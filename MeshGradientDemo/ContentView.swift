a06aa47aa4d8d8b50c0f6aa8a289a4a8aad04f2f
İhsan <ihsan.akbay@etiya.com>
2024-12-07 16:21:30 +0300

    initial commit

--- MeshGradientDemo/ContentView.swift
+++ MeshGradientDemo/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating: Bool = false
+
+    let gradientColors = (0..<9).map { _ in
+        [Color.blue, .orange, .cyan].randomElement() ?? .blue
+    }
+    let gradientColors2 = (0..<9).map { _ in
+        [Color.blue, .red, .cyan].randomElement() ?? .blue
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MeshGradient(
+            width: 3,
+            height: 3,
+            points: [
+                .init(x: 0, y: 0), .init(x: 0.5, y: 0), .init(x: 1, y: 0),
+                .init(x: 0, y: 0.5), .init(x: 0.5, y: 0.5), .init(x: 1, y: 0.5),
+                .init(x: 0, y: 1), .init(x: 0.5, y: 1), .init(x: 1, y: 1)
+            ],
+            colors: isAnimating ? gradientColors : gradientColors2
+        )
+        .ignoresSafeArea()
+        .onAppear {
+            withAnimation(.easeInOut(duration: 2).repeatForever()) {
+                isAnimating.toggle()
+            }
         }
-        .padding()
     }
 }
 
